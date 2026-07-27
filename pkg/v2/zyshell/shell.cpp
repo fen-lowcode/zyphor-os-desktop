5941446b0d408e051460b2acc975da6796e7ee02
fen <223019650+fen-lowcode@users.noreply.github.com>
2026-07-27 17:13:36 +0800

    found the bug it was just a type in the map key value field i forgot to inlude '=' when specifiying the key

--- pkg/v2/zyshell/shell.cpp
+++ pkg/v2/zyshell/shell.cpp
@@ -10,8 +10,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]]char* argv[]) {
    std::map<std::string, std::string> os_info;
 
    os_info.insert(os_info_fetcher.get_os_info("PRETTY_NAME="));
-   os_info.insert(os_info_fetcher.get_os_info("VERSION_ID"));
-   os_info.insert(os_info_fetcher.get_os_info("VERSION"));
+   os_info.insert(os_info_fetcher.get_os_info("ID="));
+   os_info.insert(os_info_fetcher.get_os_info("VERSION="));
 
    std::cout << os_info["PRETTY_NAME="] << '\n' << os_info["ID="] << '\n' << os_info["VERSION="];
    return 0;

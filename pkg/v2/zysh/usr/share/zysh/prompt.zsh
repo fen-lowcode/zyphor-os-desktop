7666da723f960e70d7d2c840825fcf77d87627c5
markjasonespelita <markjasonespelita02@gmail.com>
2026-09-01 11:53:19 +0800

    feat: update zysh prompt

--- pkg/v2/zysh/usr/share/zysh/prompt.zsh
+++ pkg/v2/zysh/usr/share/zysh/prompt.zsh
@@ -28,5 +28,5 @@ configure_prompt() {
 configure_prompt
 
 figlet -f term "$(grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2- | tr -d '"')" | lolcat
-printf "\033[1;37mLatest Build: %s\033[0m\n" "2026.08.29-r43"
+printf "\033[1;37mLatest Build: %s\033[0m\n" "2026.09.01-r44"
 echo ""
